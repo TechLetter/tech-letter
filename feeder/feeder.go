d9eeae5508dd9a9c57290c7ba82b298f791dd7dd
dhkimxx <dhkimxx@naver.com>
2025-09-23 00:13:16 +0900

    Fix: 우아한 형제들 블로그 인증서 신뢰 문제 발생

--- feeder/feeder.go
+++ feeder/feeder.go
@@ -1,6 +1,8 @@
 package feeder
 
 import (
+	"crypto/tls"
+	"net/http"
 	"time"
 
 	"github.com/mmcdole/gofeed"
@@ -15,7 +17,15 @@ type RssFeedItem struct {
 // FetchRssFeeds fetches RSS feeds from the given URL.
 // If limit is greater than 0, it returns only the first limit items.
 func FetchRssFeeds(rssUrl string, limit int) ([]RssFeedItem, error) {
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // SSL 인증서 검증을 건너뛰는 설정 , ex) 우아한 형제들
+		},
+	}
+
 	fp := gofeed.NewParser()
+	fp.Client = httpClient
+
 	feed, err := fp.ParseURL(rssUrl)
 	if err != nil {
 		return nil, err
@@ -23,7 +33,6 @@ func FetchRssFeeds(rssUrl string, limit int) ([]RssFeedItem, error) {
 
 	var items []RssFeedItem
 	for _, item := range feed.Items {
-
 		var published time.Time
 		if item.PublishedParsed != nil {
 			published = *item.PublishedParsed
